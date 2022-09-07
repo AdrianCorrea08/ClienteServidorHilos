ac6d1582562d1754b893a3655f848856ecf0bd36
Norbey Adrian Correa Narvaez <ingenierodesarrollo3@comersantander.local>
2022-09-07 17:25:15 -0500

    Se agrega el proyecto de hilos.

---
+++ src/com/example/PurchasingAgent.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example;
+
+/**
+ *
+ * @author ingenierodesarrollo3
+ */
+//clase PurchasingAgent
+public class PurchasingAgent {
+    
+    //método purchase
+    public void purchase(){
+        Store store = Store.getInstance();
+    }
+    
+}
+
+
