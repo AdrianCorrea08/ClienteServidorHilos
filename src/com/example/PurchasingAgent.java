6cc9a43d7bcc3cd9e610f782135c18c125e2c83b
AdrianCorrea08 <adrian.correa@utp.edu.co>
2022-09-09 17:55:12 -0500

    Se agrega metodo en a clase PurchasingAgent.

--- src/com/example/PurchasingAgent.java
+++ src/com/example/PurchasingAgent.java
@@ -13,9 +13,23 @@ public class PurchasingAgent {
     
     //método purchase
     public void purchase(){
+        //instanciamos el store
         Store store = Store.getInstance();
+        
+        if (store.getShirtCount() > 0 && store.authorizeCreditCard("1234", 15.00)) {
+            Shirt shirt = store.takeShirt(); 
+            System.out.println("The shirt is ours!"); 
+            System.out.println(shirt);
+        } else {
+            System.out.println("No shirt for you");
+        }
     }
     
-}
+    //Creacion del motodo
+    public PurchasingAgent() { 
+        System.out.println("Creating a purchasing agent");
+        Thread t = Thread.currentThread();
+        System.out.println("Thread:" + t.getName() + "," + t.getId());
+    } // cierre del metodo
 
-
+} // cierre de la clase
